a2aa18bf714f8c98603e8c547fec93ee5c88b3e5
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-01-24 10:01:45 +0530

    Question 1 is added

---
+++ JavaScript_Questions_Solutions/Q1.js
@@ -0,0 +1,13 @@
+// Q.1 What are the possible ways to create objects in JavaScript
+
+// There many ways to create an object in JavaScript some of them are:
+const obj1 = {
+    Name: "Himanshu Paliwal",
+    Age: 20,
+}
+
+const obj2 = new Object();
+
+const obj3 = Object();
+
+const obj4 = Object.create(null);
