8e4bf65f4d4ba006235360bf3b571e9a47904f52
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-17 22:15:40 +0530

    Question 4 is added

---
+++ JavaScript_Questions_Solutions/Q4.js
@@ -0,0 +1,19 @@
+// Q.4 What is JSON and its common operations
+
+// JavaScript Object Notation (JSON) is a standard text-based format for representing structured data based on JavaScript object syntax. It is commonly used for transmitting data in web applications (e.g., sending some data from the server to the client, so it can be displayed on a web page, or vice versa)
+
+// Common JSON Operations
+
+// Parsing: Converting a JSON string to a native object. JSON parsing makes it possible to take a JSON string received from a server or a file and convert it into a JavaScript object.
+
+// Stringification: Converting a native object to a JSON string. This operation is useful when you need to send data from a client to a server as part of an HTTP request.
+
+// Querying: Accessing parts of the JSON object to retrieve specific data. Once you have parsed a JSON string into an object, you can query the object for data using the dot notation or bracket notation.
+
+// Modification: After parsing JSON into a native object, you can easily modify its properties.
+
+// Deletion: Similarly, you can delete properties from a JSON object using the delete keyword in JavaScript.
+
+// Traversal: For more complex JSON objects, such as those containing nested arrays or objects, you might need to traverse the JSON structure using loops or recursive functions to access all the data it contains.
+
+// Validation: Ensuring that the JSON data is correctly formatted according to a predefined schema or format.
